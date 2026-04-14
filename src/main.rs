163819bc0ff7c258788fa7ebab7aaa28cf4e4638
Nick Repin <nickrepin@gmail.com>
2026-04-14 17:28:56 +1000

    ditto

--- src/main.rs
+++ src/main.rs
@@ -161,27 +161,27 @@ fn main() {
         sleep_millis
     );
 
-    if false && sleep_millis > 0 {
+    if sleep_millis > 0 {
         let sleep_duration = std::time::Duration::from_millis(sleep_millis.into());
         std::thread::sleep(sleep_duration);
     };
 
     // 4 Play
     match out_config.sample_format() {
-        // cpal::SampleFormat::I8 => run::<i8>(input_wav, &out_device, out_config.into()),
-        // cpal::SampleFormat::I16 => run::<i16>(input_wav, &out_device, out_config.into()),
-        // cpal::SampleFormat::I24 => run::<I24>(input_wav, &out_device, out_config.into()),
-        // cpal::SampleFormat::I32 => run::<i32>(input_wav, &out_device, out_config.into()),
-        // // cpal::SampleFormat::I48 => run::<I48>(input_wav, &out_device, config.into()),
-        // cpal::SampleFormat::I64 => run::<i64>(input_wav, &out_device, out_config.into()),
-        // cpal::SampleFormat::U8 => run::<u8>(input_wav, &out_device, out_config.into()),
-        // cpal::SampleFormat::U16 => run::<u16>(input_wav, &out_device, out_config.into()),
-        // cpal::SampleFormat::U24 => run::<U24>(input_wav, &out_device, out_config.into()),
-        // cpal::SampleFormat::U32 => run::<u32>(input_wav, &out_device, out_config.into()),
-        // // cpal::SampleFormat::U48 => run::<U48>(input_wav, &out_device, config.into()),
-        // cpal::SampleFormat::U64 => run::<u64>(input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::I8 => run::<i8>(&mut input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::I16 => run::<i16>(&mut input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::I24 => run::<I24>(&mut input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::I32 => run::<i32>(&mut input_wav, &out_device, out_config.into()),
+        // cpal::SampleFormat::I48 => run::<I48>(&mut input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::I64 => run::<i64>(&mut input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::U8 => run::<u8>(&mut input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::U16 => run::<u16>(&mut input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::U24 => run::<U24>(&mut input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::U32 => run::<u32>(&mut input_wav, &out_device, out_config.into()),
+        // cpal::SampleFormat::U48 => run::<U48>(&mut input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::U64 => run::<u64>(&mut input_wav, &out_device, out_config.into()),
         cpal::SampleFormat::F32 => run::<f32>(&mut input_wav, &out_device, out_config.into()),
-        // cpal::SampleFormat::F64 => run::<f64>(input_wav, &out_device, out_config.into()),
+        cpal::SampleFormat::F64 => run::<f64>(&mut input_wav, &out_device, out_config.into()),
         sample_format => panic!("Unsupported sample format '{sample_format}'"),
     }.unwrap();
     
@@ -216,6 +216,37 @@ fn convert_sample_rate(data: &Vec<f32>, input_sample_rate: u32, out_sample_rate:
     .unwrap_or_default()
 }
 
+fn convert_channels(in_samples: &Vec<f32>, in_channels: usize, out_channels: usize) -> Vec<f32> {
+    if in_channels == out_channels {
+        in_samples.clone()
+    } else if in_channels == 1 {
+        // 1 -> 2 - clone left
+        println!("Dupe to stereo");
+        let mut new_samples : Vec<f32> = Vec::new();
+        for sample in in_samples.iter() {
+            new_samples.push(*sample);
+            new_samples.push(*sample);
+        }
+        new_samples
+    } else if in_channels == 2 {
+        // 2 -> 1 - average
+        // mix down stereo to mono
+        println!("Mix to mono");
+        in_samples
+            .chunks(2) // Iterate over pairs of samples (left, right)
+            .map(|chunk| {
+                let left = chunk[0];
+                let right = chunk[1];
+                let mono = (left + right) / 2.0; // Average the two channels
+                (mono * i16::MAX as f32).round() as f32
+            })
+            .collect()
+    } else {
+        assert!(false);
+        in_samples.clone()
+    }
+}
+
 fn get_modulus(arg_speed: Speed) -> u32 {
     match arg_speed {
         Speed::Slow => 30,
@@ -237,21 +268,25 @@ where
     for sample in input_wav.samples::<i16>() {
         input_wav_f32.push(f32::from_sample::<i16>(sample.unwrap()));
     }
-    let mut input_wav_f32_resampled = convert_sample_rate(
+    let input_wav_f32_resampled = convert_sample_rate(
         &input_wav_f32, input_spec.sample_rate, out_config.sample_rate, 1
     );
-    let mut in_samples = input_wav_f32_resampled.into_iter();
 
+    let  input_wav_f32_resampled = convert_channels(
+        & input_wav_f32_resampled, 
+        input_spec.channels as usize, 
+        out_config.channels as usize
+    );
+
+    let mut in_samples = input_wav_f32_resampled.into_iter();
     let in_sample_count = in_samples.len();
     println!("in_sample_count: {:?}", in_sample_count);
 
     let out_sample_rate = out_config.sample_rate as f32;
 
-    let out_channels = out_config.channels as usize;
-
     let out_data_fn = move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
         let mut input_fell_behind = false;
-        for frame in data.chunks_mut(2) {
+        for frame in data.chunks_mut(out_config.channels as usize) {
             let signal_value = match in_samples.next() {
                 Some(s) => s,
                 None => {
